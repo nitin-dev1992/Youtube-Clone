60799318ab210c7789419951cefbe760fd40fd7b
Nitin Rana <nitinksshy72@gmail.com>
2026-09-14 17:39:29 +0530

    Register Controllers

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,59 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/apiResponse.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { fullName, username, email, password } = req.body
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === '')
+    ) {
+        throw new ApiError(400, "All fields are required!")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username exists!")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required!")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar is required!")
+    }
+
+    const user = await User.create({
+        fullName,
+        username: username.toLowerCase(),
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password
+    })
+
+    const createdUser = await User.findById(user._id)
+        .select(
+            "-password -refreshToken"
+        )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user!")
+    }
+
+    return res.status(201).json(new ApiResponse(200, createdUser, "User registered successfully"))
+})
+
+export { registerUser }
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,4 +1,4 @@
-class APiError extends Error {
+class ApiError extends Error {
     constructor(
         statusCode,
         message = "Something went wrong",
@@ -19,3 +19,4 @@ class APiError extends Error {
         }
     }
 }
+export { ApiError }
