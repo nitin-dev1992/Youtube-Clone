ff298d221aa8acf2d5c4293437e29ce04123ad6a
Nitin Rana <nitinksshy72@gmail.com>
2026-09-17 12:16:10 +0530

    Refactor logoutUser to use $unset for refreshToken; add Comment, Like, Playlist, and Tweet models

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -13,4 +13,4 @@ const subscriptionSchema = new Schema({
 
 
 
-export const Subscription = mongoose.model("subscription", subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
